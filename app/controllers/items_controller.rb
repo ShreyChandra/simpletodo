9f1b4e01ceffc068f3f23999fb9d4ebec0215f45
Shrey Chandra <shrey.2012.chandra@gmail.com>
2016-09-18 18:11:40 +0000

    Added edit and Destroy actions adn View files

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,11 @@
 class ItemsController < ApplicationController
-    
+    before_action :find_item, only: [:show, :edit, :update, :destroy]
     def index
     @items = Item.all.order("created_at DESC")
     end
     
     def show
-       @item = Item.find(params[:id]) 
+       
     end
     
     def new 
@@ -21,10 +21,31 @@ class ItemsController < ApplicationController
         end
     end
     
+    def edit
+    end
+    
+    def update
+        if @item.update(item_params)
+            redirect_to item_path(@item)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @item.destroy
+        
+        redirect_to root_path
+    end
+    
     private
     
     def item_params
         params.require(:item).permit(:title, :description)
+    end
+    
+    def find_item
+    @item = Item.find(params[:id])
     end
 
 end
